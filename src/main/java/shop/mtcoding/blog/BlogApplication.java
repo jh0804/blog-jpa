85418cce0c9330c85a0360a9baf7a438188f263f
jay <wngml0708@gmail.com>
2025-04-23 14:00:46 +0900

    filter

--- src/main/java/shop/mtcoding/blog/BlogApplication.java
+++ src/main/java/shop/mtcoding/blog/BlogApplication.java
@@ -2,7 +2,9 @@ package shop.mtcoding.blog;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 
+@ServletComponentScan
 @SpringBootApplication
 public class BlogApplication {
 
